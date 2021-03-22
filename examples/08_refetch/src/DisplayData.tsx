92fd24f903c5927f13048ae7651a9f69a35db768
Volkan Unsal <spocksplanet@gmail.com>
2021-03-21 21:48:26 -0400

    Add example for refetch

---
+++ examples/08_refetch/src/DisplayData.tsx
@@ -0,0 +1,44 @@
+// eslint-disable-next-line spaced-comment
+/// <reference types="react/experimental" />
+
+import React, {
+  unstable_useTransition as useTransition,
+  useState,
+} from 'react';
+
+import { UserData } from './fetchFuncs';
+
+type Props = {
+  result: UserData;
+  update: (first_name: string) => void;
+};
+
+const DisplayData: React.FC<Props> = ({ result, update }) => {
+  const [startTransition, isPending] = useTransition();
+  const [first_name, setFirstName] = useState(result.data.first_name);
+  const onClick = () => {
+    startTransition(() => {
+      update(first_name);
+    });
+  };
+  return (
+    <div>
+      <div>
+        First Name:{' '}
+        <input
+          type="text"
+          value={first_name}
+          onChange={(e) => {
+            setFirstName(e.target.value);
+          }}
+        />
+      </div>
+      <button type="button" onClick={onClick}>
+        Update
+      </button>
+      {isPending && 'Pending...'}
+    </div>
+  );
+};
+
+export default DisplayData;
---
+++ examples/08_refetch/src/Item.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+import { store, mutate } from './fetchFuncs';
+import DisplayData from './DisplayData';
+
+type Props = {
+  initialId: string;
+};
+
+const Item: React.FC<Props> = ({ initialId }) => {
+  const [text, setText] = useState(initialId);
+  const result = store.get(initialId);
+  const update = (first_name: string) => {
+    mutate(initialId, first_name).then(() => {
+      store.refetch(initialId);
+    });
+  };
+  return (
+    <div>
+      User ID: <input value={text} onChange={(e) => setText(e.target.value)} />
+      <DisplayData result={result} update={update} />
+    </div>
+  );
+};
+
+export default Item;
